f96a0e397e50a1e05b578ab29a8f732e766ea58c
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-03-20 23:22:32 +0700

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar";
+import logo from "../logo.svg";
+import "../css/App.css";
+import Navbar from "./Navbar";
 function App() {
   return (
     <div className="App">
